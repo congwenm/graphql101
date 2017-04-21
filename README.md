ec94621a4852b18964601d53391870b2cde6782f
Fei <lostaznfound@gmail.com>
2017-04-20 23:30:03 -0500

    express

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Getting started with GraphQL.js
 http://graphql.org/graphql-js/
+
+## graphiQL is a great tool for debuggin and inspecting a server, recommend using it in dev mode.
+
+
+http://graphql.org/graphql-js/graphql-clients/
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var { graphql, buildSchema } = require('graphql')
+var express = require('express')
+var graphqlHTTP = require('express-graphql')
+var { buildSchema } = require('graphql')
 
 // construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -10,11 +12,16 @@ var schema = buildSchema(`
 // The root provides a resolver function for each APi endpoint
 var root = {
   hello: () => {
-    return 'Hello world!'
+    return 'Hello worlds!'
   }
 }
 
-// Run the GraphQL query '{ hello }` and print out the response
-graphql(schema, '{ hello }', root).then(
-  response => console.log(response)
-)
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}))
+
+app.listen(4000)
+console.log('Running a GraphQL API server at localhost:4000/graphql')
